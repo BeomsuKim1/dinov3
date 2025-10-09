9612173c45f33ed512af40a6c44c972209ba95dd
BeomsuKim <greentree53@naver.com>
2025-10-10 05:43:02 +0900

    -

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 import torch
 
+os.environ["TORCH_HOME"] = "/home/beomsu/ssd/torch_cache"
 REPO_DIR = "/home/beomsu/dinov3/dinov3"
+WEIGHTS_DIR = "/home/beomsu/ssd/dinov3_weights/"
 
-dinov3_vith16plus = torch.hub.load(REPO_DIR, 'dinov3_vith16plus', source='local', weights="/home/beomsu/ssd/dinov3_weights/dinov3_vith16plus_pretrain_lvd1689m")
-#dinov3_vit7b16 = torch.hub.load(REPO_DIR, 'dinov3_vit7b16', source='local', weights=<CHECKPOINT/URL/OR/PATH>)
+dinov3_vith16plus = torch.hub.load(REPO_DIR, 'dinov3_vith16plus', source='local', weights=WEIGHTS_DIR + 'dinov3_vith16plus_pretrain_lvd1689m-7c1da9a5.pth')
+dinov3_vit7b16 = torch.hub.load(REPO_DIR, 'dinov3_vit7b16', source='local', weights=WEIGHTS_DIR + 'dinov3_vit7b16_pretrain_lvd1689m-a955f4ea.pth')
