d91870c67da4a8345f6f39a8a79b8d8f5b64307e
BeomsuKim <greentree53@naver.com>
2025-10-10 04:48:50 +0900

    -

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 
-REPO_DIR = "/home/beomsu/dinov3/dinov3"
+REPO_DIR = "/home/beomsu/dinov3/dinov3[torch]"
 
-dinov3_vith16plus = torch.hub.load(REPO_DIR, 'dinov3_vith16plus', source='local', weights=<CHECKPOINT/URL/OR/PATH>)
-dinov3_vit7b16 = torch.hub.load(REPO_DIR, 'dinov3_vit7b16', source='local', weights=<CHECKPOINT/URL/OR/PATH>)
+dinov3_vith16plus = torch.hub.load(REPO_DIR, 'dinov3_vith16plus', source='local', weights="/home/beomsu/ssd/dinov3_weights/dinov3_vith16plus_pretrain_lvd1689m")
+#dinov3_vit7b16 = torch.hub.load(REPO_DIR, 'dinov3_vit7b16', source='local', weights=<CHECKPOINT/URL/OR/PATH>)
